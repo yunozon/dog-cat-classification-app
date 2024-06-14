026071193add1d3f216884b0652a356e1495ad09
yunozon <guru.fu.kamie21@gmail.com>
2024-06-14 20:42:08 +0900

    update

---
+++ work/main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+print("Hello World!")
+print("Hello World!")
+print('12345')
